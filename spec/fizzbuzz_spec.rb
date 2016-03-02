d935fa8c556ffec1c6411cbcd4adc2258d2c1574
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-03-03 10:55:32 +1100

    Using let because let is cool.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,10 @@
 require './app/fizzbuzz'
 
 describe 'fizzbuzz' do
-  describe 'when input is indivisible by 3 and 5' do
-    subject { fizzbuzz(4) }
+  subject { fizzbuzz(input) }
 
+  describe 'when input is indivisible by 3 and 5' do
+    let(:input) { 4 }
     it { is_expected.to eq '4' }
   end
 end
